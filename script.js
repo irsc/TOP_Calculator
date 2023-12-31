75f148cd0ac42a74ed7f21945c0534cb303a57b0
irsc <rivero.scastilla@gmail.com>
2023-12-31 18:40:16 +0100

    Minimum operations working now

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // Calcuator functions
 const add = function(a,b) {
-	return a + b;
+    const numA = +a;
+    const numB = +b;
+	return numA + numB;
 };
 
 const substract = function(a, b) {
@@ -19,6 +21,7 @@ const divide = function(a, b){
 let firstNumber ="";
 let secondNumber="";
 let  operator;
+let decimal = false;
 
 const operation = function(a, b, operator){
     switch (operator) {
@@ -36,26 +39,65 @@ const operation = function(a, b, operator){
     }
 };
 
+const clearAll = function(){
+    input.textContent = "";
+    result.textContent = "";
+    operator = "";
+    firstNumber = "";
+    secondNumber = "";
+    decimal = false;
+};
+
+const getNumbers = function(e){
+    if(!operator){
+        firstNumber += e.target.dataset.number;
+    }else if(firstNumber && operator){
+        secondNumber += e.target.dataset.number;
+    }
+    input.textContent += e.target.dataset.number;
+};
+
+const getParameters = function(op, first, second, dec){
+    operator = op;
+    firstNumber = first;
+    secondNumber = second;
+    decimal = dec;
+}
+
 //EventListeners
 
 const btn = document.querySelector(".buttonPad");
+const btnAC = document.querySelector("#AC");
 const input =document.querySelector(".inputText");
 const result = document.querySelector(".resultText");
 
 btn.addEventListener("click",(e)=>{
 
     if(e.target.dataset.number){
-        input.textContent += e.target.dataset.number;
-        if(!operator){
-            firstNumber += e.target.dataset.number;
-        }else if(firstNumber && operator){
-            secondNumber += e.target.dataset.number;
+        if(result.textContent && !operator){
+            clearAll();
+        }
+        if(e.target.dataset.number != "."){
+            getNumbers(e);
+        }
+        if(e.target.dataset.number == "." && !decimal){
+            getNumbers(e);
+            decimal = true;
         }
     }
 
-    if(e.target.dataset.operator){
+    if(e.target.dataset.operator && e.target.dataset.operator != "equal" && e.target.dataset.operator != "minus"){
         if(!operator){
             operator = e.target.dataset.operator;
+            input.textContent += e.target.textContent;
+            decimal = false;
+        }else if(firstNumber && operator && secondNumber){
+            result.textContent = operation(firstNumber, secondNumber, operator);
+            getParameters(e.target.dataset.operator,result.textContent,"",false);
+            /* operator = e.target.dataset.operator;
+            firstNumber = result.textContent;
+            secondNumber = "";
+            decimal = false; */
             input.textContent += e.target.textContent;
         }
     }
@@ -63,10 +105,27 @@ btn.addEventListener("click",(e)=>{
     if(e.target.dataset.operator == "equal"){
         if(firstNumber && operator && secondNumber){
             result.textContent = operation(firstNumber, secondNumber, operator);
+            /* operator = "";
+            firstNumber = result.textContent;
+            secondNumber = "";
+            decimal = false; */
+            getParameters("",result.textContent,"",false);
+        }
+    }
+
+    if(e.target.dataset.operator == "minus"){
+        if(!firstNumber){
+            input.textContent+="-";
+            firstNumber ="-";
+        }else if(operator && !secondNumber){
+            input.textContent +="-";
+            secondNumber="-";
         }
     }
 
     
 });
+
+btnAC.addEventListener("click",()=>clearAll());
 
 
